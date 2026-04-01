133e0937254e6ca218bb615ede49535026ff30f6
Donald Hays <donald.hays@gmail.com>
2026-03-31 19:44:00 -0700

    Squashed 'lib/iui/' changes from 59877be..c8c0a25
    c8c0a25 Added MSDF Image support
    c959aaa Migrated fill mode logic to utils
    5e4d520 Added checkmark glyph
    
    git-subtree-dir: lib/iui
    git-subtree-split: c8c0a25efed5d55ee5d5cbbb3092df0467cb394d

--- utils.lua
+++ utils.lua
@@ -42,4 +42,91 @@ function utils.clamp(n, low, high)
     return n
 end
 
+--- @param iw number
+--- @param ih number
+--- @param x number
+--- @param y number
+--- @param w number
+--- @param h number
+--- @return number x, number y, number w, number h
+function utils.aspectFit(iw, ih, x, y, w, h)
+    local aspectBounds = w / h
+    local aspectImage = iw / ih
+
+    local ox, oy, ow, oh = x, y, w, h
+
+    if aspectImage > aspectBounds then
+        oh = iui.utils.round(w / aspectImage)
+        oy = y + iui.utils.round((h - oh) / 2)
+    else
+        ow = iui.utils.round(h * aspectImage)
+        ox = x + iui.utils.round((w - ow) / 2)
+    end
+
+    return ox, oy, ow, oh
+end
+
+--- @param iw number
+--- @param ih number
+--- @param x number
+--- @param y number
+--- @param w number
+--- @param h number
+--- @return number x, number y, number w, number h
+function utils.aspectFill(iw, ih, x, y, w, h)
+    local aspectBounds = w / h
+    local aspectImage = iw / ih
+
+    local ox, oy, ow, oh = x, y, w, h
+
+    if aspectImage < aspectBounds then
+        oh = iui.utils.round(ow / aspectImage)
+        oy = y + iui.utils.round((h - oh) / 2)
+    else
+        ow = iui.utils.round(oh * aspectImage)
+        ox = x + iui.utils.round((w - ow) / 2)
+    end
+
+    return ox, oy, ow, oh
+end
+
+--- @param iw number
+--- @param ih number
+--- @param x number
+--- @param y number
+--- @param w number
+--- @param h number
+--- @return number x, number y, number w, number h
+function utils.center(iw, ih, x, y, w, h)
+    local ox, oy, ow, oh = x, y, w, h
+
+    ow, oh = iw, ih
+    ox = x + iui.utils.round((w - ow) / 2)
+    oy = y + iui.utils.round((h - oh) / 2)
+
+    return ox, oy, ow, oh
+end
+
+--- @param mode IUIImageMode
+--- @param iw number
+--- @param ih number
+--- @param x number
+--- @param y number
+--- @param w number
+--- @param h number
+--- @return number x, number y, number w, number h
+function utils.fill(mode, iw, ih, x, y, w, h)
+    if mode == "fill" then
+        return x, y, w, h
+    elseif mode == "aspectFit" then
+        return iui.utils.aspectFit(iw, ih, x, y, w, h)
+    elseif mode == "aspectFill" then
+        return iui.utils.aspectFill(iw, ih, x, y, w, h)
+    elseif mode == "center" then
+        return iui.utils.center(iw, ih, x, y, w, h)
+    else
+        error("unrecognized fillmode")
+    end
+end
+
 iui.utils = utils
--- widgets/msdf-image.lua
+++ widgets/msdf-image.lua
@@ -4,16 +4,16 @@ local parentPath = currentPath:match('(.-)[^%./]+%.$')
 --- @class IUILib
 local iui = require(parentPath .. "iui")
 
---- @alias IUIImageMode "fill" | "aspectFit" | "aspectFill" | "center"
-
 --- @param image any
-function iui.image(image)
+--- @param color? IUIColor
+function iui.msdfImage(image, color)
+    color = color or iui.colors.sysGray1000
+
     local bx, by, bw, bh = iui.layout.getBounds()
     local iw, ih = iui.graphics.getImageDimensions(image)
 
-    local filter = iui.style["imageFilter"] or "linear" --- @type IUIImageFilter
-    local mode = iui.style["imageMode"] or "aspectFit"  --- @type IUIImageMode
-    local clip = iui.style["imageClip"] or false        --- @type boolean
+    local mode = iui.style["imageMode"] or "aspectFit" --- @type IUIImageMode
+    local clip = iui.style["imageClip"] or false       --- @type boolean
 
     local ox, oy, ow, oh = iui.utils.fill(mode, iw, ih, bx, by, bw, bh)
 
@@ -22,8 +22,8 @@ function iui.image(image)
     end
 
     iui.draw(function()
-        iui.graphics.setColor(1, 1, 1)
-        iui.graphics.image(image, filter, ox, oy, ow, oh)
+        color:set()
+        iui.graphics.msdfImage(image, ox, oy, ow, oh)
     end)
 
     if clip then
