ab3d2bb35504bd43782cde977c59dafebfcaf541
Donald Hays <donald.hays@gmail.com>
2026-03-31 17:06:24 -0700

    Migrated fill mode logic to utils

--- lib/iui/widgets/image.lua
+++ lib/iui/widgets/image.lua
@@ -11,46 +11,11 @@ function iui.image(image)
     local bx, by, bw, bh = iui.layout.getBounds()
     local iw, ih = iui.graphics.getImageDimensions(image)
 
-    local aspectBounds = bw / bh
-    local aspectImage = iw / ih
-
-    local ox, oy, ow, oh = bx, by, bw, bh
-
     local filter = iui.style["imageFilter"] or "linear" --- @type IUIImageFilter
     local mode = iui.style["imageMode"] or "aspectFit"  --- @type IUIImageMode
     local clip = iui.style["imageClip"] or false        --- @type boolean
 
-    if mode == "fill" then
-        -- No changes necessary
-    elseif mode == "aspectFit" then
-        if aspectImage > aspectBounds then
-            ox = bx
-            ow = bw
-            oh = iui.utils.round(ow / aspectImage)
-            oy = by + iui.utils.round((bh - oh) / 2)
-        else
-            oy = by
-            oh = bh
-            ow = iui.utils.round(oh * aspectImage)
-            ox = bx + iui.utils.round((bw - ow) / 2)
-        end
-    elseif mode == "aspectFill" then
-        if aspectImage < aspectBounds then
-            ox = bx
-            ow = bw
-            oh = iui.utils.round(ow / aspectImage)
-            oy = by + iui.utils.round((bh - oh) / 2)
-        else
-            oy = by
-            oh = bh
-            ow = iui.utils.round(oh * aspectImage)
-            ox = bx + iui.utils.round((bw - ow) / 2)
-        end
-    elseif mode == "center" then
-        ow, oh = iw, ih
-        ox = bx + iui.utils.round((bw - ow) / 2)
-        oy = by + iui.utils.round((bh - oh) / 2)
-    end
+    local ox, oy, ow, oh = iui.utils.fill(mode, iw, ih, bx, by, bw, bh)
 
     if clip then
         iui.draw.pushClip(bx, by, bw, bh)
