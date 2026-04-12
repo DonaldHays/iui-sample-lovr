3f862fc6bc79dde3e68799984d944fd9cba7f071
Donald Hays <donald.hays@gmail.com>
2026-04-11 23:45:10 -0700

    Squashed 'lib/iui/' changes from 43b6b65..c112d39
    c112d39 ListView respects spacing
    
    git-subtree-dir: lib/iui
    git-subtree-split: c112d3948b816afe9d404b98d30dbe3869c7b608

--- widgets/list-view.lua
+++ widgets/list-view.lua
@@ -29,6 +29,9 @@ function iui.listView(name, count, rowHeight, itemBuilder, manager)
     local state = iui.state(id)
 
     local margin = iui.style["margin"]
+    local spacing = iui.style["spacing"]
+
+    local spaced = rowHeight + spacing
 
     if not manager then
         manager = state.manager
@@ -40,23 +43,27 @@ function iui.listView(name, count, rowHeight, itemBuilder, manager)
     end
 
     iui.scrollView("ScrollView", function()
+        if count == 0 then
+            return
+        end
+
         local panel = iui.layout.getPanel()
         local yOffset = iui.utils.round(manager.y)
 
         panel.contentHeight = math.max(
-            panel.contentHeight, count * rowHeight + margin * 2 - yOffset
+            panel.contentHeight, count * spaced - spacing + margin * 2 - yOffset
         )
 
         local minIndex = math.max(
-            math.floor((yOffset - margin) / rowHeight) + 1, 1
+            math.floor((yOffset - margin) / spaced) + 1, 1
         )
 
         local maxIndex = math.min(
-            math.ceil((yOffset + panel.h - margin) / rowHeight), count
+            math.ceil((yOffset + panel.h - margin) / spaced), count
         )
 
         for index = minIndex, maxIndex do
-            panel.rowY = (index - 1) * rowHeight + margin - yOffset
+            panel.rowY = (index - 1) * spaced + margin - yOffset
             iui.layout.beginRow({ kind = "dynamic", count = 1 }, rowHeight)
             itemBuilder(index)
         end
